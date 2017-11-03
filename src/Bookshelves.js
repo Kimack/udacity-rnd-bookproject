3f7804f502648524cc94ad7f50ae5a03f6ed019c
Nathan Robbert <admin@bigrocketgames.com>
2017-11-02 22:08:13 -0700

    feat: Refactored BookList into its own class component.

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -1,33 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const BookList = ({book}) => {
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              
-              {book.shelf === "currentlyReading" ? <option value="currentlyReading" selected>Currently Reading</option> : <option value="currentlyReading">Currently Reading</option> }
-              
-              {book.shelf === "wantToRead" ? <option value="wantToRead" selected>Want to Read</option> : <option value="wantToRead">Want to Read</option>}
-              
-              {book.shelf === "read" ? <option value="read" selected>Read</option> : <option value="read">Read</option>}
-              
-              {!book.shelf ? <option value="none" selected>None</option> : <option value="none">None</option>}
-            
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors.join(", ")}</div>
-      </div>
-    </li>
-  )  
-}
+import BookDisplayCard from './BookDisplayCard'
+
 
 class Bookshelves extends Component {
   render() {
@@ -54,7 +29,7 @@ class Bookshelves extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {currentlyReadingShelf && currentlyReadingShelf.map((book) => <BookList key={book.id} book={book} />) }
+                  {currentlyReadingShelf && currentlyReadingShelf.map((book) => <BookDisplayCard key={book.id} book={book} updateBook={this.props.updateBook} />) }
                 </ol>
               </div>
             </div>
@@ -64,7 +39,7 @@ class Bookshelves extends Component {
               <h2 className="bookshelf-title">Want To Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {wantToReadShelf && wantToReadShelf.map((book) => <BookList key={book.id} book={book} />) }
+                  {wantToReadShelf && wantToReadShelf.map((book) => <BookDisplayCard key={book.id} book={book} updateBook={this.props.updateBook} />) }
                 </ol>
               </div>
             </div>
@@ -74,7 +49,7 @@ class Bookshelves extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {readShelf && readShelf.map((book) => <BookList key={book.id} book={book} />) }
+                  {readShelf && readShelf.map((book) => <BookDisplayCard key={book.id} book={book} updateBook={this.props.updateBook} />) }
                 </ol>
               </div>
             </div>
