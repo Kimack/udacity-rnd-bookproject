745bec5e929729b7534f65f60563b4d91ae28b2b
Nathan Robbert <admin@bigrocketgames.com>
2017-11-02 22:05:44 -0700

    Connected BookSearch component to call the BooksAPI.search upon typing and display the results using the BookDisplayCard as used on the Bookshelves component.

--- src/Booksearch.js
+++ src/Booksearch.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import BookDisplayCard from './BookDisplayCard'
+
 class BookSearch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      queryString: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      queryString: e.target.value
+    })
+    this.props.bookSearch(this.state.queryString)
+  }
+  
   render() {
+    const { books } = this.props
+
     return(
       <div className="search-books">
         <div className="search-books-bar">
@@ -16,12 +37,14 @@ class BookSearch extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author" onChange={this.handleChange}/>
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+          {books && books.map((book) => <BookDisplayCard key={book.id} book={book} updateBook={this.props.updateBook} />) }
+          </ol>
         </div>
       </div>
     )
